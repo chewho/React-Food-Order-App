53878076b09019038232bb3b16b8a36c03242928
chewho <molocrips@gmail.com>
2023-07-28 16:34:50 +0200

    feat: add a cart context

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -7,7 +7,7 @@ const MealItemForm = (props) => {
       <Input
         label="Amount"
         input={{
-          id: "amount" + props.id,
+          id: "amount_" + props.id,
           type: "number",
           min: "1",
           max: "5",
